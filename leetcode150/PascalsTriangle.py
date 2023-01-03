9ba9f2897b508c62636b61954de1bfb2ff381c9a
jayPreak <jayesh.preak2003@gmail.com>
2023-01-03 15:19:30 +0530

    PascalsTriangle

---
+++ leetcode150/PascalsTriangle.py
@@ -0,0 +1,19 @@
+# n = 6
+# res = []
+# for i in range(n):
+#     x = [int(x) for x in str(11**i)]
+#     res.append(x)
+
+# print(res)
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        a = []
+        for x in range(numRows):
+            a.append([1])
+            if x > 1:
+                for c in range(x-1):
+                    a[x].append(a[x-1][c]+a[x-1][c+1])
+            if x > 0:
+                a[x].append(1)
+        return a
