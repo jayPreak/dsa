2954e95e1466af358e7bc1e1f4b9d6fecf187db7
jayPreak <jayesh.preak2003@gmail.com>
2023-01-15 11:49:34 +0530

    SinglerNumber

---
+++ leetcode150/SinglerNumber.py
@@ -0,0 +1,8 @@
+from collections import Counter
+ 
+# initializing string
+test_str = "GeeksforGeeks"
+ 
+# using collections.Counter() to get
+# count of each element in string
+res = Counter(test_str)
