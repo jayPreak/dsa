44f0c6794fa2ae02061bdc7ffdf14220b9eb48bb
jayPreak <jayesh.preak2003@gmail.com>
2023-01-15 11:59:33 +0530

    SinglerNumber

--- leetcode150/SinglerNumber.py
+++ leetcode150/SinglerNumber.py
@@ -1,8 +1,18 @@
+# from collections import Counter
+ 
+# # initializing string
+# test_str = [1, 2, 3, 4, 5, 6, 1]
+ 
+# # using collections.Counter() to get
+# # count of each element in string
+# res = Counter(test_str)
+# z = res.most_common()
+# print(z[-1][0])
+
 from collections import Counter
- 
-# initializing string
-test_str = "GeeksforGeeks"
- 
-# using collections.Counter() to get
-# count of each element in string
-res = Counter(test_str)
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        a = Counter(nums)
+        b = a.most_common()
+        return b[-1][0]
