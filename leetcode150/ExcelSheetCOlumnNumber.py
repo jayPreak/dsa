484766371dde4bdb0509bd56d21b8e4340597362
jayPreak <jayesh.preak2003@gmail.com>
2023-01-28 12:46:10 +0530

    ExcelSheetCOlumnNumber

--- leetcode150/ExcelSheetCOlumnNumber.py
+++ leetcode150/ExcelSheetCOlumnNumber.py
@@ -1,7 +1,17 @@
-x = "CDA"
-res = 0
-for i in x:
-    res *= 26
-    res += ord(i) - 64
+# x = "CDA"
+# res = 0
+# for i in x:
+#     res *= 26
+#     res += ord(i) - 64
 
-print(res)
+# print(res)
+
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        res = 0
+        for i in columnTitle:
+            res *= 26
+            res += ord(i) - 64
+
+        return res
