66768b6dccdd8a31ca589f172d6d67ee4ac75c3d
jayPreak <jayesh.preak2003@gmail.com>
2023-01-30 11:11:52 +0530

    number1bits

--- leetcode150/Number1Bits.py
+++ leetcode150/Number1Bits.py
@@ -1,1 +1,22 @@
-bin(n)[2:]
+# n = 561
+# n = str(bin(n)[2:])
+# count = 0
+# for i in n:
+#     if i == "1":
+#         count+=1
+
+# print(count)
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        n = str(bin(n)[2:])
+        c = 0
+        for i in n:
+            if i == "1":
+                c+=1
+
+        return c
+
+        # another sol
+
+        return bin(n).count("1")
