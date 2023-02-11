bba51531ebf93f1fb454f0fa10b8d89069d66754
jayPreak <jayesh.preak2003@gmail.com>
2023-02-11 12:23:50 +0530

    FirstUniqueCharinString

---
+++ leetcode150/FirstUniqueCharinString.py
@@ -0,0 +1,13 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        m = {}
+        for i in s:
+            if i not in m:
+                m[i] = 1
+            else:
+                m[i] += 1
+        # index = -1
+        for i in range(len(s)):
+            if m[s[i]] == 1:
+                return i
+        return -1
