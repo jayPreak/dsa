47dbf7b974b9c9f931c3c650331d0e990f775c1a
jayPreak <jayesh.preak2003@gmail.com>
2023-02-16 21:17:59 +0530

    com

---
+++ codeforces/feb16/A.py
@@ -0,0 +1,94 @@
+from re import L
+import sys
+import os.path
+from math import *
+from bisect import *
+from itertools import *
+from collections import defaultdict as dd, Counter
+from itertools import accumulate
+from operator import add
+#from sortedcontainers import SortedList as SL, SortedSet as SS, SortedDict as SD
+
+if os.path.exists("input.txt"):
+    sys.stdin = open("input.txt", "r")
+    sys.stdout = open("output.txt", "w")
+
+def st():
+    return str(sys.stdin.buffer.readline().strip())[2:-1]
+def li():
+    return list(map(int, sys.stdin.buffer.readline().split()))
+def mp():
+    return map(int, sys.stdin.buffer.readline().split())
+def inp():
+    return int(sys.stdin.buffer.readline())
+    
+def printCleanList(x):
+    print(*x, sep=" ")
+
+def printListToStr(x):
+    print(*x, sep="")
+
+def checkBeau(a):
+    i = 0
+    j = 1
+    beau1 = True
+    while True:
+        if i == len(a) - 1:
+            break
+        if a[i] == a[j]:
+            beau1 = False
+            # print(a[i], "yes")
+            break
+        i+=1
+        j+=1
+    return beau1
+
+mod = 1000000007
+INF = float("inf")
+
+def solve():
+    # print()
+    n, m = mp()
+    a = st()
+    b = st()
+
+    # ans = False
+
+    checkedA = checkBeau(a)
+    checkedB = checkBeau(b)
+    
+
+
+    if checkedA and checkedB:
+        print("YES")
+    else:
+        if checkedA:
+            if a[-1] == b[-1]:
+                print("NO")
+            else:
+
+        if checkedB:
+            if checkBeau(a[:-1]) and a[-1] != b[-1]:
+                print("YES")
+            else:
+                print("NO")
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+    
+    # ---- CHECK FOR CORNER CASES ----
+    
+for _ in range(inp()):
+    solve()
