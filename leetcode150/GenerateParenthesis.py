527319c6c85f8f3c14b5fb2f3a8ca383ab8e928a
jayPreak <jayesh.preak2003@gmail.com>
2023-03-06 10:30:19 +0530

    GenerateParenthesis XD

---
+++ leetcode150/GenerateParenthesis.py
@@ -0,0 +1,35 @@
+# n = int(input())
+# res = []
+# def dfs(left, right, s):
+#     if len(s) == n*2:
+#         res.append(s)
+#         return
+    
+#     if left < n:
+#         dfs(left+1, right, s +'(')
+#     if right < left:
+#         dfs(left, right + 1, s+')')
+    
+
+# dfs(0, 0, '')
+# print(res)
+    
+
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def dfs(left, right, s):
+            if len(s) == n*2:
+                res.append(s)
+                return
+            
+            if left < n:
+                dfs(left+1, right, s +'(')
+            if right < left:
+                dfs(left, right + 1, s+')')
+            
+
+        dfs(0, 0, '')
+        return res
