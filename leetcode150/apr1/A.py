a5bad55fe4082ad7ed4836006578ea3be7aef7db
jayPreak <jayesh.preak2003@gmail.com>
2023-04-01 21:29:37 +0530

    kms

--- leetcode150/apr1/A.py
+++ leetcode150/apr1/A.py
@@ -1,45 +1,22 @@
-from re import L
-import sys
-import os.path
-from math import *
-from bisect import *
-from itertools import *
-from collections import defaultdict as dd, Counter
-from itertools import accumulate
-from operator import add
-#from sortedcontainers import SortedList as SL, SortedSet as SS, SortedDict as SD
-
-if os.path.exists("input.txt"):
-    sys.stdin = open("input.txt", "r")
-    sys.stdout = open("output.txt", "w")
-
-def st():
-    return str(sys.stdin.buffer.readline().strip())[2:-1]
-def li():
-    return list(map(int, sys.stdin.buffer.readline().split()))
-def mp():
-    return map(int, sys.stdin.buffer.readline().split())
-def inp():
-    return int(sys.stdin.buffer.readline())
-    
-def printCleanList(x):
-    print(*x, sep=" ")
-
-def printListToStr(x):
-    print(*x, sep="")
-
-mod = 1000000007
-INF = float("inf")
-
-def solve():
-    print()
-
-
-
-
-
-    
-    # ---- CHECK FOR CORNER CASES ----
-    
-for _ in range(inp()):
-    solve()
+class Solution:
+    def maximumCostSubstring(self, s: str, chars: str, vals: List[int]) -> int:
+        res = 0
+        y = ord("a")-1
+        if s == chars and sum(vals)<0:
+            return res
+        else:
+            for i in range(len(s)):
+                for j in range(i, len(s)+1):
+                    temp = 0
+                    # print(i, "yep", j)
+                    # print(s[i:j])
+                    for x in s[i:j]:
+                        
+                        if x in chars:
+                            temp += vals[chars.index(x)]
+                        else:
+                            temp+= ord(x) - y
+                    res = max(res, temp)
+                    
+            return res
+                    
