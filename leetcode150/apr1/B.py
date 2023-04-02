e85c7c78babd39a19c3a565cefa863c3c643348e
jayPreak <jayesh.preak2003@gmail.com>
2023-04-02 08:52:41 +0530

    :/

---
+++ leetcode150/apr1/B.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumCostSubstring(self, s: str, chars: str, vals: List[int]) -> int:
+        m = dict(zip(chars,vals))
+        res = cur = 0
+        for c in s:
+            cur = max(cur + m.get(c, ord(c)-ord("a")+1), 0)
+            res = max(cur, res)
+        return res
