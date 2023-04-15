f5ea7882658fc8fcd2f68a0d715b37631674641c
jayPreak <jayesh.preak2003@gmail.com>
2023-04-15 10:38:14 +0530

    hm pretty?

--- leetcode150/wordinword.py
+++ leetcode150/wordinword.py
@@ -1,13 +1,14 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def dfs(board, word, i, j , k):
-            if not 0 <= i < len(board) or not 0 <= j <len(board[0]) or board[i][j] != word[k]:
+        def dfs(board, word, i, j, k):
+            if not 0 <= i < len(board) or not 0 <= j < len(board[0]) or board[i][j] != word[k]:
                 return False
             if k == len(word) - 1:
                 return True
             tmp, board[i][j] = board[i][j], '/'
             res = dfs(board, word, i + 1, j, k + 1) or dfs(board, word, i - 1, j, k + 1) or \
-                dfs(board, word, i, j + 1, k + 1) or dfs(board, word, i, j - 1, k + 1)
+                dfs(board, word, i, j + 1, k +
+                    1) or dfs(board, word, i, j - 1, k + 1)
             board[i][j] = tmp
             return res
         for i in range(len(board)):
@@ -15,5 +16,3 @@ class Solution:
                 if dfs(board, word, i, j, 0):
                     return True
         return False
-        
-        
