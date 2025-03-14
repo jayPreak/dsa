6438374f28904bff12f0acce497a950d0ea7fb98
jaypreak <jayesh.preak2003@gmail.com>
2025-03-14 19:35:25 +0530

    yum

--- 2025/python/test.py
+++ 2025/python/test.py
@@ -1,12 +1,15 @@
-nums = [1,2,3,4]
-flag = False
-temp=set()
-
+nums = [4,1,2,1,2]
+hash = {}
+ 
 for num in nums:
-    if num in temp:
-        flag = True
+    
+    if num in hash:
+        hash[num] += 1
+    else:
+        hash[num] = 1
+        
+for key in hash:
+    if hash[key] == 1:
+        print(key)
         break
-    temp.add(num)
-    
-print(flag)
-    
+     
