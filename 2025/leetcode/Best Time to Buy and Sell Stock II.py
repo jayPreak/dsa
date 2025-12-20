18ad8ad5827849dc5773758b61cc670070fcf63b
jaypreak <jayesh.preak2003@gmail.com>
2025-12-20 16:51:26 +0530

    meow123

--- 2025/leetcode/Best Time to Buy and Sell Stock II.py
+++ 2025/leetcode/Best Time to Buy and Sell Stock II.py
@@ -7,4 +7,14 @@ class Solution:
                 
         return profit
 
-        
+# well i got worse
+#  class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         res = []
+#         for i in range(0, len(prices)-1):
+#             if prices[i] < prices[i+1]:
+#                 res.append(prices[i+1] - prices[i])
+
+#         return sum(res)
+                
+               
