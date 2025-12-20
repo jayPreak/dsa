c2bb794ec1cec9e9c8f6abbfc4471572992996b0
jaypreak <jayesh.preak2003@gmail.com>
2025-12-20 15:07:53 +0530

    meow

--- old/leetcode150/RemoveDuplicatesFromSortedArray.py
+++ old/leetcode150/RemoveDuplicatesFromSortedArray.py
@@ -10,13 +10,24 @@
 #     nums[i] = m[i]
 
 # print(nums)
+#my 3yo solution 😭
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         m = list(set(nums))
+#         m.sort()
+#         for i in range(len(m)):
+#             nums[i] = m[i]
+            
+#         return len(m)
+
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        m = list(set(nums))
-        m.sort()
-        for i in range(len(m)):
-            nums[i] = m[i]
-            
-        return len(m)
+        j = 1
+        for i in range(1, len(nums)):
+            # print(i)
+            if nums[i] != nums[i-1]:
+                nums[j] = nums[i]
+                j += 1
+        return j
         
