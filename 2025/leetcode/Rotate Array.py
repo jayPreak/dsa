871054623a10928a30f59a2068cbb95983c2a016
jaypreak <jayesh.preak2003@gmail.com>
2025-12-21 17:03:41 +0530

    i hate myself ngl

--- 2025/leetcode/Rotate Array.py
+++ 2025/leetcode/Rotate Array.py
@@ -1,4 +1,15 @@
+# class Solution:
+#     def rotate(self, nums: List[int], k: int) -> None:
+#         for i in range(k):
+#             nums.insert(0, nums.pop(-1))
+
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         for i in range(k):
-            nums.insert(0, nums.pop(-1))
+            x = nums.pop()
+            nums.insert(0, x)
+        
