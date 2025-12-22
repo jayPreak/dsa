81d34de022076d9393dc50daed5d0056408fcf4e
jaypreak <jayesh.preak2003@gmail.com>
2025-12-22 15:07:53 +0530

    :( im getting worse

--- 2025/leetcode/Contains Duplicates.py
+++ 2025/leetcode/Contains Duplicates.py
@@ -1,14 +1,22 @@
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         flag = False
+
+#         temp=set()
+
+#         for num in nums:
+#             if num in temp:
+#                 flag = True
+#                 break
+#             temp.add(num)
+            
+#         return flag
+        
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        flag = False
-
-        temp=set()
-
-        for num in nums:
-            if num in temp:
-                flag = True
-                break
-            temp.add(num)
-            
-        return flag
-        
+        res = set(nums)
+        if len(res) == len(nums):
+            return False
+        else:
+            return True
