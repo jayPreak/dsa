c9a1aca155b2629a9cabf4b56f3753c8b6389413
jaypreak <jayesh.preak2003@gmail.com>
2025-12-26 23:25:07 +0530

    meow im getting better!

--- old/leetcode150/IntersectionOfTwoArrays.py
+++ old/leetcode150/IntersectionOfTwoArrays.py
@@ -1,11 +1,30 @@
-from collections import Counter
+# from collections import Counter
+# class Solution:
+#     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         c1 = Counter(nums1)
+#         c2 = Counter(nums2)
+#         res = []
+#         for key in c1.keys() & c2.keys():
+#             res.extend([key]*min(c1[key], c2[key]))
+
+#         return res
+        
+
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        c1 = Counter(nums1)
-        c2 = Counter(nums2)
+        nums1.sort()
+        nums2.sort()
+        i = 0
+        j = 0
         res = []
-        for key in c1.keys() & c2.keys():
-            res.extend([key]*min(c1[key], c2[key]))
-
-        return res
         
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i+=1
+            elif nums1[i] > nums2[j]:
+                j+=1
+            else:
+                res.append(nums1[i])
+                i+=1
+                j+=1
+        return res
--- old/leetcode150/PlusOne.py
+++ old/leetcode150/PlusOne.py
@@ -1,8 +1,38 @@
+# class Solution:
+#     def plusOne(self, digits: List[int]) -> List[int]:
+#         a = int("".join(map(str, digits)))
+#         a += 1
+#         digits = list(map(int, str(a)))
+#         return digits
+                
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        a = int("".join(map(str, digits)))
-        a += 1
-        digits = list(map(int, str(a)))
+        i = -1
+        while True:
+            if digits[i] != 9:
+                digits[i] += 1
+                break
+            else:
+                print(f'i: {i}')
+                print(f'0: {digits[0]}, digits i: {digits[i]}')
+                if digits[0] == digits[i]:
+                    digits[i] = 0
+                    meow = len(digits)*-1
+                    print(f'digits : {digits}, len(digits): {meow} AND i = {i}')
+                    if len(digits) * -1 == i:
+                        digits.insert(0, 1)
+                        break 
+                    i-=1
+                else:
+                    digits[i] = 0
+                    if len(digits) * -1 == i:
+                        break
+                    i -= 1
+                    
+                    
+                    
+        print(digits)
         return digits
-                
-                
+                    
+                    
+        
